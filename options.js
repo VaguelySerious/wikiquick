92eb6301a5e1c5470b9445baa94899ed47616df7
Peter Wielander <peter@17grad.com>
2018-06-05 17:23:34 +0200

    Trying to get double click working

--- options.js
+++ options.js
@@ -1,9 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
 var page = document.getElementById('buttonDiv');
 var kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 
